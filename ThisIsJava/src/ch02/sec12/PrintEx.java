66654477a8343b195f52d82b573d6f5bf5fb53de
KOO612 <ksm99007@gmail.com>
2024-04-24 18:04:51 +0900

    0424 이것이자바다

---
+++ ThisIsJava/src/ch02/sec12/PrintEx.java
@@ -0,0 +1,11 @@
+package ch02.sec12;
+
+public class PrintEx {
+    public static void main(String[] args) {
+        int value = 123;
+        System.out.printf("상품의 가격:%d원\n", value);
+        System.out.printf("상품의 가격:%");
+        System.out.printf("상품의 가격:%");
+        System.out.printf("상품의 가격:%");
+    }
+}
