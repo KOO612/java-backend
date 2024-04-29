32f37b89a4af9ef67829be9501d43dbe17003bc1
KOO612 <ksm99007@gmail.com>
2024-04-29 18:04:31 +0900

    0429 이것이자바다

---
+++ ThisIsJava/src/ch03/AccurayEx.java
@@ -0,0 +1,12 @@
+package ch03;
+
+public class AccurayEx {
+    public static void main(String[] args) {
+        int apple = 1;
+        double piece = 0.1;
+        int num = 7;
+
+        double result = apple - num * piece;
+        System.out.println("사과 1개에서 남은 양 : " + result);
+    }
+}
